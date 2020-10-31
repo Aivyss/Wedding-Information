3ad5bdeaef367eef9d646c8924beef4342ea9522
DESKTOP-1QE2BEU\USER <rlaxodud9028@gmail.com>
2020-10-31 12:34:58 +0900

    남자 vo

--- WeddingInformation/src/vo/Male.java
+++ WeddingInformation/src/vo/Male.java
@@ -3,9 +3,20 @@ package vo;
 import java.util.ArrayList;
 
 public class Male extends Human {
+	boolean taco = false;
 	
 	public Male(ArrayList<Object> humanInfo) {
 		super(humanInfo);
+		this.taco = taco;
 	}
 
+	public boolean isTaco() {
+		return taco;
+	}
+
+	public void setTaco(boolean taco) {
+		this.taco = taco;
+	}
+	
+	
 }
