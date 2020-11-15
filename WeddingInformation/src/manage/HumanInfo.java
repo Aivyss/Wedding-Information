5576a810931fa5ac505891274df071ad71e4b789
DESKTOP-DKO3O52\hklei <hklee6417@gmail.com>
2020-11-16 03:17:29 +0900

    최종본 2차

--- WeddingInformation/src/manage/HumanInfo.java
+++ WeddingInformation/src/manage/HumanInfo.java
@@ -1,15 +1,11 @@
 package manage;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import dao.WeddingDAO;
-import vo.Female;
 import vo.Human;
-import vo.Male;
 
 public class HumanInfo {
 	private Random rd;
@@ -23,7 +19,7 @@ public class HumanInfo {
 		rd = new Random();
 		dao = new WeddingDAO();
 	}
-	
+
 	/**
 	 * HumanInfo 객체를 얻는 메소드
 	 */
@@ -31,8 +27,8 @@ public class HumanInfo {
 		return humanInfo;
 	}
 
-	/** (완료)
-	 * 로그인을 수행하는 메소드이다.
+	/**
+	 * (완료) 로그인을 수행하는 메소드이다.
 	 * 
 	 * @param id
 	 * @param pw
@@ -44,19 +40,19 @@ public class HumanInfo {
 
 		if (vo != null) {
 			if (vo.getLockCount() < 3) {
-				if (!pw.equals(vo.getPassword())) { 
+				if (!pw.equals(vo.getPassword())) {
 					// 비밀번호를 틀려 계정락 카운트를 늘리고 로그인 실패
 					vo.setLockCount(vo.getLockCount() + 1);
 					dao.updateLockCount(vo);
 					vo = null;
 				}
-			} else { 
+			} else {
 				// 비밀번호를 3회 이상 틀려 계정락상태, 로그인 실패
 				vo = null;
 			}
 		}
 
-		if (vo != null) { 
+		if (vo != null) {
 			// 로그인에 성공한 경우 락카운트 초기화 및 등급이름 호출
 			vo.setLockCount(0);
 			dao.updateLockCount(vo);
@@ -65,8 +61,8 @@ public class HumanInfo {
 		return vo;
 	}
 
-	/** (완료)
-	 * id로 회원을 찾아서 vo객체를 반환하는 메소드
+	/**
+	 * (완료) id로 회원을 찾아서 vo객체를 반환하는 메소드
 	 * 
 	 * @param id
 	 * @return 회원을 찾지 못할시에는 null을 반환함
@@ -83,14 +79,14 @@ public class HumanInfo {
 	 */
 	public boolean addAccount(Human vo) {
 		boolean flag = false;
-		
-		flag = (dao.searchAccount(vo.getId())==null) ? dao.addAccount(vo) : false;
+
+		flag = (dao.searchAccount(vo.getId()) == null) ? dao.addAccount(vo) : false;
 
 		return flag;
 	}
 
-	/** (완료)
-	 * 로그인한 고객이 금액을 충전할 수 있도록 수행하는 메소드
+	/**
+	 * (완료) 로그인한 고객이 금액을 충전할 수 있도록 수행하는 메소드
 	 * 
 	 * @param vo
 	 * @param pw
@@ -103,15 +99,15 @@ public class HumanInfo {
 		if (pw.equals(vo.getPassword())) {
 			vo.setCash(vo.getCash() + cash);
 			dao.updateCash(vo);
-			
+
 			flag = true; // 입금처리 성공
 		}
 
 		return flag;
 	}
 
-	/** (완료)
-	 * 지정한 레벨인 대상자를 랜덤으로 뽑아내는 메소드 리스트로 해당자만 담아서 그 안에서 랜덤으로 추출한다.
+	/**
+	 * (완료) 지정한 레벨인 대상자를 랜덤으로 뽑아내는 메소드 리스트로 해당자만 담아서 그 안에서 랜덤으로 추출한다.
 	 * 
 	 * @param level
 	 * @return
@@ -120,17 +116,17 @@ public class HumanInfo {
 		List<Human> list = null;
 		List<Human> candidate = new ArrayList<Human>();
 		Human matched = new Human();
-		
+
 		if (vo.getSex() == 1) {
 			matched.setSex(0);
 		} else {
 			matched.setSex(1);
 		}
-		
+
 		list = dao.getList(matched); // 이성을 골라냄
-		
+
 		for (Human vvo : list) {
-			if(vvo.getGradeIndex() == vo.getGradeIndex()) {
+			if (vvo.getGradeIndex() == vo.getGradeIndex()) {
 				candidate.add(vvo); // 같은 등급만 골라냄
 			}
 		}
@@ -145,8 +141,8 @@ public class HumanInfo {
 		return matched;
 	}
 
-	/** (완료)
-	 * 매칭요청을 수행하는 메소드
+	/**
+	 * (완료) 매칭요청을 수행하는 메소드
 	 * 
 	 * @param me
 	 * @param you
@@ -158,17 +154,19 @@ public class HumanInfo {
 		Human you = dao.searchAccount(yourId);
 
 		if (me != null && you != null) {
-			me.setMatchedId(you.getId());
-			me.setInvited(0);
-			me.setMatchLock(1);
-			dao.updateLockAndMatch(me);
+			if (me.getMatchLock() == 0 && you.getMatchLock() == 0) {
+				me.setMatchedId(you.getId());
+				me.setInvited(0);
+				me.setMatchLock(1);
+				dao.updateLockAndMatch(me);
 
-			you.setMatchedId(me.getId());
-			you.setInvited(1);
-			you.setMatchLock(1);
-			dao.updateLockAndMatch(you);
+				you.setMatchedId(me.getId());
+				you.setInvited(1);
+				you.setMatchLock(1);
+				dao.updateLockAndMatch(you);
 
-			flag = true;
+				flag = true;
+			}
 		}
 
 		return flag;
@@ -182,7 +180,7 @@ public class HumanInfo {
 		Human you = dao.searchAccount(yourId);
 		boolean flagT = false;
 
-		if (me.getInvited()==1 && me.getSuccess()!=1) {
+		if (me.getInvited() == 1 && me.getSuccess() != 1) {
 			int gradeIndex = me.getGradeIndex();
 
 			int fee = (gradeIndex == 0) ? 100000
@@ -198,7 +196,10 @@ public class HumanInfo {
 					you.setInvited(1);
 					you.setSuccess(1);
 					me.setSuccess(1);
-
+					
+					dao.updateLockAndMatch(me);
+					dao.updateLockAndMatch(you);
+					
 					flagT = true;
 				} else { // 돈이 없어 매칭이 실패
 					initialize(myId);
@@ -211,8 +212,8 @@ public class HumanInfo {
 		return flagT;
 	}
 
-	/** (완료)
-	 * 매칭 정보를 초기화 해주는 메소드. 다시 만남을 가질 수 있도록 해준다.
+	/**
+	 * (완료) 매칭 정보를 초기화 해주는 메소드. 다시 만남을 가질 수 있도록 해준다.
 	 * 
 	 * @param id
 	 * @return
@@ -223,15 +224,15 @@ public class HumanInfo {
 		Human you = searchAccount(me.getMatchedId());
 
 		if (me != null && you != null) {
-			me.setMatchedId(null);
+			me.setMatchedId("");
 			me.setInvited(0);
 			me.setMatchLock(0);
-			you.setMatchedId(null);
+			me.setSuccess(0);
+			you.setMatchedId("");
 			you.setInvited(0);
 			you.setMatchLock(0);
 			you.setSuccess(0);
-			me.setSuccess(0);
-			
+
 			dao.updateLockAndMatch(me);
 			dao.updateLockAndMatch(you);
 
@@ -241,24 +242,19 @@ public class HumanInfo {
 		return flag;
 	}
 
-	/** (완료)
-	 * 회원 탈퇴를 진행하는 메소드 회원탈퇴가 성공하면 true 실패하면 false
+	/**
+	 * (완료) 회원 탈퇴를 진행하는 메소드 회원탈퇴가 성공하면 true 실패하면 false
 	 * 
 	 * @param id
 	 * @return
 	 */
 	public boolean removeAccount(String id, String pw) {
 		boolean flag = false;
-		
+
 		Human vo = dao.searchAccount(id);
 		if (pw.equals(vo.getPassword())) {
 			initialize(id); // 매칭 상대가 있다면 매칭 상대도 상태를 반영해야 하므로 시행
 			flag = dao.deleteAccount(vo);
-			
-			if (flag) {
-				ScoreRank.giveGrade(dao.searchAccount(id)); // 삭제된 회원에따른 등급 재배치
-			}
-			
 		}
 
 		return flag;
